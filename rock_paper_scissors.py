a81d4ab1b8256c764aa46797865b4a78d119c82e
Utku Hasbey Kurban <69817471+Liduvo@users.noreply.github.com>
2021-12-21 01:02:58 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,83 +2,114 @@ import random
 
 rock_paper_scissors_v1 = ["rock","paper","scissors"]
 rock_paper_scissors_v2 = ["rock","gun","lightning","devil","dragon","water","air","paper","sponge","wolf","tree","human","snake","scissors","fire"]
-player2_or_computer = " "
-computer_or_human = 0
+
+print("****************************************************")
+print("                Rock, Paper,Scissors                ")
+print("V1 = rock, paper and scissors")
+print("V2 = rock, gun, lightning, devil, dragon, water, air, paper, sponge, wolf, tree, human, snake, scissors and fire")
+print("****************************************************")
+v1_or_v2 = input("     V1 or V2 :").lower()
 
 
-def rock_paper_scissorcs_v1(computer_or_human):
+
+if v1_or_v2 == "v1":
+
     while True:
+        print("---------------------------------------------------")
+        print("Player1 Choice")
+        player = input("rock,paper or scissors? :")
+        print("---------------------------------------------------")
 
-        player = input("rock,paper or scissors?:")
-        if computer_or_human == 1:
-            player2_or_computer = random.choice(rock_paper_scissors_v1)
+        while player not in rock_paper_scissors_v1:
+            print("---------------------------------------------------")
+            print("Please choose between rock, paper and scissors.")
+            print("Player1 Choice")
+            player = input("rock,paper or scissors?")
+            print("---------------------------------------------------")
 
-        elif computer_or_human == 2:
-            player2_or_computer = input("rock, paper, or scissors?:")
+        computer = random.choice(rock_paper_scissors_v1)
 
-            while player2_or_computer not in rock_paper_scissors_v1:
-                player2_or_computer = input("rock, paper, or scissors?: ")
+        if player == computer:
+            print("---------------------------------------------------")
+            print("                       Tie!                        ")
+            print("---------------------------------------------------")
 
-        if player == player2_or_computer:
-                print("Tie!")
+        elif player == rock_paper_scissors_v1[0]:
+            if computer == rock_paper_scissors_v1[1]:
+                print("---------------------------------------------------")
+                print("Computer Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[1])
+                print("Player choice: " + rock_paper_scissors_v1[0])
+                print("---------------------------------------------------")
 
+            elif computer == rock_paper_scissors_v1[2]:
+                print("---------------------------------------------------")
+                print("Player Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[2])
+                print("Player choice:" + rock_paper_scissors_v1[0])
+                print("---------------------------------------------------")
 
-                if player == rock_paper_scissors_v1[0]:
-                    if player2_or_computer == rock_paper_scissors_v1[1]:
-                        print("=============================")
-                        print("Win Computer")
-                        print("Computer choices: " + player2_or_computer)
-                        print("You choices: " + player)
-                        print("=============================")
+        elif player == rock_paper_scissors_v1[1]:
+            if computer == rock_paper_scissors_v1[0]:
+                print("---------------------------------------------------")
+                print("Player Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[0])
+                print("Player choice: " + rock_paper_scissors_v1[1])
+                print("---------------------------------------------------")
 
-                    elif player2_or_computer == rock_paper_scissors_v1[2]:
-                        print("=============================")
-                        print("Win Player")
-                        print("Computer choices: " + player2_or_computer)
-                        print("You choices: " + player)
-                        print("=============================")
+            elif computer == rock_paper_scissors_v1[2]:
+                print("---------------------------------------------------")
+                print("Computer Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[2])
+                print("Player choice: " + rock_paper_scissors_v1[1])
+                print("---------------------------------------------------")
 
-                elif player == rock_paper_scissors_v1[1]:
-                    if player2_or_computer == rock_paper_scissors_v1[0]:
-                        print("=============================")
-                        print("Win Player")
-                        print("Computer choices: " + player2_or_computer)
-                        print("You choices: " + player)
-                        print("=============================")
+        elif player == rock_paper_scissors_v1[2]:
+            if computer == rock_paper_scissors_v1[0]:
+                print("---------------------------------------------------")
+                print("Computer Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[0])
+                print("Player choice: " + rock_paper_scissors_v1[2])
+                print("---------------------------------------------------")
 
-                    elif player2_or_computer == rock_paper_scissors_v1[2]:
-                        print("=============================")
-                        print("Win Computer")
-                        print("Computer choices: " + player2_or_computer)
-                        print("You choices: " + player)
-                        print("=============================")
-
-
-                elif player == rock_paper_scissors_v1[2]:
-                    if player2_or_computer == rock_paper_scissors_v1[0]:
-                        print("=============================")
-                        print("Win Computer")
-                        print("Computer choice: " + player2_or_computer)
-                        print("You choice: " + player)
-                        print("=============================")
-
-                    elif player2_or_computer == rock_paper_scissors_v1[1]:
-                        print("=============================")
-                        print("Win Player")
-                        print("Computer choice: " + player2_or_computer)
-                        print("You choice: " + player)
-                        print("=============================")
-
-            play_again = input("Play again? (yes/no): ").lower()
-            print("**********************************************")
-
-            if play_again != "yes":
-                break
-                print("See you again.")
+            elif computer == rock_paper_scissors_v1[1]:
+                print("---------------------------------------------------")
+                print("Player Win!")
+                print("Computer choice: " + rock_paper_scissors_v1[1])
+                print("Player choice: " + rock_paper_scissors_v1[2])
+                print("---------------------------------------------------")
 
 
+        print("Play again?")
+        play_again = input("     Yes or No :").lower()
 
-rock_paper_scissorcs_v1()
+        if play_again != "yes":
+            print("See you again.")
+            break
 
 
+
+
+elif v1_or_v2 == "v2":
+    while True:
+        print("---------------------------------------------------")
+        print("Player1 Choice")
+        player = input("rock, gun, lightning, devil, dragon, water, air, paper, sponge, wolf, tree, human, snake, scissors, fire? :")
+        print("---------------------------------------------------")
+
+        while player not in rock_paper_scissors_v2:
+            print("---------------------------------------------------")
+            print("Please choose between rock, gun, lightning, devil, dragon, water, air, paper, sponge, wolf, tree, human, snake, scissors and fire.")
+            print("Player1 Choice")
+            player = input("rock, gun, lightning, devil, dragon, water, air, paper, sponge, wolf, tree, human, snake, scissors and fire?")
+            print("---------------------------------------------------")
+
+        computer = random.choice(rock_paper_scissors_v2)
+
+        if player == computer:
+            print("---------------------------------------------------")
+            print("                       Tie!                        ")
+            print("---------------------------------------------------")
+
+        elif player == rock_paper_scissors_v2[0]:
 
